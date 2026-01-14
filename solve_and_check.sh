0ec05893a44e567ee8327ee661d13d992280e4bb
Yoni Zohar <yoni206@gmail.com>
2026-01-14 03:10:54 +0000

    remove commented out code

--- solve_and_check.sh
+++ solve_and_check.sh
@@ -7,6 +7,5 @@ PROOF=`realpath /tmp/tmp.smt2.alethe`
 
 tee "$BENCHMARK" | $CVC5_BIN -i --solve-bv-as-int=sum --dump-proofs --proof-format=alethe >  $PROOF.tmp
 cat $PROOF.tmp | tail -n+3 | head -n-1 > $PROOF
-# tee "$BENCHMARK" | $CVC5_BIN -i --solve-bv-as-int=sum --dump-proofs --proof-format=alethe | tail -n+3 | head -n-1 > $PROOF
 $CARCARA_BIN check --expand-let-bindings -i $PROOF
 exit $?
